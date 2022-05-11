0bfbd7168a1b9a6803a52d1ffd441cc9d7c68315
Ubuntu <azureuser@D16asV4.1v0ylmuf3afe1bj4xyiczy3wrf.bx.internal.cloudapp.net>
2022-05-11 02:59:42 +0000

    fastrnn compute

--- onnxruntime/contrib_ops/cpu/fastrnn.cc
+++ onnxruntime/contrib_ops/cpu/fastrnn.cc
@@ -26,8 +26,109 @@
 namespace onnxruntime {
 namespace contrib {
 
+void Gemm(CBLAS_TRANSPOSE trans_a, CBLAS_TRANSPOSE trans_b,
+                          int64_t M, int64_t N, int64_t K,
+                          float alpha,
+                          const float* a_data, const float* b_data,
+                          float beta,
+                          const float* c_data, const TensorShape* c_shape,
+                          float* y_data,
+                          concurrency::ThreadPool* thread_pool) {
+  // if input is empty tensor, return directly as nothing need to be calculated.
+  if (M == 0 || N == 0)
+    return;
+
+  math::Gemm<float>(trans_a, trans_b,
+                M, N, K,
+                alpha,
+                a_data,
+                b_data,
+                // ideally we need to set the output buffer contents to 0 if bias is missing,
+                // but passing 0 for beta is cheaper and it will ignore any junk in the output buffer
+                c_data != nullptr ? beta : 0,
+                y_data,
+                thread_pool);
+}
+
 Status FastRNN::Compute(OpKernelContext* context) const {
-    return Status(onnxruntime::common::ONNXRUNTIME, onnxruntime::common::FAIL, "");
+    const auto* X = context->Input<Tensor>(0);
+    // const auto* hiddenStates = context->Input<Tensor>(1);
+    const auto* W = context->Input<Tensor>(2);
+    const auto* U = context->Input<Tensor>(3);
+    // const auto* bias_gate = context->Input<Tensor>(4);
+    // const auto* bias_update = context->Input<Tensor>(5);
+    // const auto* zeta = context->Input<Tensor>(6);
+    // const auto* nu = context->Input<Tensor>(7);
+
+
+    // Dimensions
+    int64_t sequence = X->Shape()[1];
+    int64_t in_chs = X->Shape()[2];
+    int64_t out_chs = W->Shape()[1];
+
+    // output
+    std::vector<int64_t> Y_dims({1, sequence, out_chs});
+    Tensor* Y = context->Output(0, Y_dims);
+
+    const float *Xdata = X->template Data<float>();
+    const float *Wdata = W->template Data<float>();
+    const float *Udata = U->template Data<float>();
+    float* Ydata = Y->template MutableData<float>();
+    concurrency::ThreadPool* thread_pool = context->GetOperatorThreadPool();
+
+    float *buffer  =(float*)malloc(sizeof(float) * 3 * out_chs);
+    float *wComp = buffer;
+    float *uComp = buffer + out_chs;
+    float *preComp = buffer + 2 * out_chs;
+
+    for (int i = 0; i < sequence; i++)
+    {
+      const float *Xdata_i = Xdata + i * in_chs;
+      float *Ydata_i = Ydata + i * out_chs;
+
+      int64_t M = 1;
+      int64_t N = out_chs;
+      int64_t K = in_chs;
+      TensorShape state_shape = {out_chs};
+
+      Gemm(CblasNoTrans, CblasNoTrans, M, N, K, 1.0, Xdata_i, Wdata, 1.0,
+                  nullptr, &state_shape, wComp, thread_pool);
+
+      Gemm(CblasNoTrans, CblasNoTrans, M, N, K, 1.0, Xdata_i, Udata, 1.0,
+                  nullptr, &state_shape, uComp, thread_pool);
+
+      MLAS_ACTIVATION act;
+      act.ActivationKind = MlasLogisticActivation;
+      MlasActivation(
+        &act,
+        wComp,
+        nullptr,
+        1,
+        N,
+        N
+      ); 
+
+      act.ActivationKind = MlasTanhActivation;
+      MlasActivation(
+        &act,
+        uComp,
+        nullptr,
+        1,
+        N,
+        N
+      ); 
+
+      //math::Add<float>(wComp, uComp, Ydata_i);
+      ONNX_UNUSED_PARAMETER(Ydata_i);
+    }
+
+    free(buffer);
+
+    
+    ONNX_UNUSED_PARAMETER(Udata);
+    ONNX_UNUSED_PARAMETER(uComp);
+    ONNX_UNUSED_PARAMETER(preComp);
+    return Status::OK();
 }
 
 ONNX_OPERATOR_KERNEL_EX(
